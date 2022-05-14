5c98a9a4dd5b35c1b79e5a07b07cfffef587f81b
Msalati <ItsMsalati@gmail.com>
2022-05-14 18:04:18 +0200

    more UI improvements

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,37 +1,47 @@
 export default [
   {
-    title: 'الرئيسية',
+    title: 'لوحة التحكم',
     route: 'home',
     icon: 'HomeIcon',
   },
   {
-    title: 'الطلبة',
+    title: 'قائمة الطلبة',
     route: 'second-page',
     icon: 'UsersIcon',
+  },
+  {
+    title: 'الفصول و النقاشات',
+    route: 'second-page',
+    icon: 'NoIcon',
+  },
+  {
+    title: 'جدول الدراسة و الإمتحانات',
+    route: 'second-page',
+    icon: 'NoIcon',
+  },
+  {
+    title: 'الواجبات',
+    route: 'second-page',
+    icon: 'NoIcon',
+  },
+  {
+    title: 'إدارة بنك الأسئلة',
+    route: 'second-page',
+    icon: 'NoIcon',
+  },
+  {
+    title: 'الحضور و الغياب',
+    route: 'second-page',
+    icon: 'NoIcon',
+  },
+  {
+    title: 'المحادثات',
+    route: 'second-page',
+    icon: 'ChatIcon',
   },
   {
     title: 'منشئ الإختبارات',
     route: 'second-page',
     icon: 'Edit3Icon',
-  },
-  {
-    title: 'Second Page',
-    route: 'second-page',
-    icon: 'FileIcon',
-  },
-  {
-    title: 'Second Page',
-    route: 'second-page',
-    icon: 'Crop',
-  },
-  {
-    title: 'Second Page',
-    route: 'second-page',
-    icon: 'FileIcon',
-  },
-  {
-    title: 'Second Page',
-    route: 'second-page',
-    icon: 'FileIcon',
   },
 ]
